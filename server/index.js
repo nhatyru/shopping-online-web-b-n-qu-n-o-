3d374be77d05640e01dcaaaca601dc623ff0ecb3
Nhat Nguyen <pandrapa985@gmail.com>
2026-04-09 00:33:58 +0700

    fix route admin + render

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-const dns =require('dns');
+const dns = require('dns');
 dns.setServers(['1.1.1.1','1.0.0.1']);
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -14,13 +16,17 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello from server!' });
 });
+
 app.use('/api/admin', require('./api/admin.js'));
 app.use('/api/customer', require('./api/customer.js'));
 
+// ADMIN (đặt trước)
 app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
-app.get('admin/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+app.get('/admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'));
 });
+
+// CUSTOMER (đặt sau)
 app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
